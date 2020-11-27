8979ae2f51c2a0a777e8ee8b672bf155aedfb0bf
Amit <kundu.amit517@gmail.com>
2020-11-27 15:53:31 +0600

    Recyclearview added

---
+++ app/src/main/java/com/team/myapplication/adapter/DetailsAdapter.java
@@ -0,0 +1,80 @@
+package com.team.myapplication.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.team.myapplication.Constant;
+import com.team.myapplication.R;
+import com.team.myapplication.ui.CurrentProduct;
+
+import java.lang.ref.WeakReference;
+
+public class DetailsAdapter extends RecyclerView.Adapter<DetailsAdapter.ViewHolder> {
+
+    private Context context;
+    private final DescriptionClickListener listener;
+    private int row_index = -1;
+
+    public DetailsAdapter(Context context, DescriptionClickListener listener) {
+        this.context = context;
+        this.listener = listener;
+    }
+
+    public interface DescriptionClickListener {
+        void position(int position);
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new ViewHolder(
+                LayoutInflater.from(parent.getContext()).inflate(R.layout.item_product_image_2, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+        holder.productImage.setImageResource(CurrentProduct.getCurrentProduct().getExampleArrayList().get(position).getFrontImg());
+        if(row_index==position){
+            holder.productImage.setBackground(ContextCompat.getDrawable(context, R.drawable.shape_green_image));
+        }
+        else
+        {
+            holder.productImage.setBackground(ContextCompat.getDrawable(context, R.drawable.shape_round_corner_2));
+        }
+        holder.productImage.setClipToOutline(true);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return CurrentProduct.getCurrentProduct().getExampleArrayList().size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+        private WeakReference<DescriptionClickListener> listenerRef;
+        private ImageView productImage;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            listenerRef = new WeakReference<>(listener); // Click Listener Ref
+            productImage = itemView.findViewById(R.id.productImage);
+            productImage.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            if (view.getId() == productImage.getId()){
+                row_index = getAdapterPosition();
+                listenerRef.get().position(getAdapterPosition());
+                notifyDataSetChanged();
+            }
+        }
+    }
+}
--- app/src/main/java/com/team/myapplication/ui/ProductDialogFragment.java
+++ app/src/main/java/com/team/myapplication/ui/ProductDialogFragment.java
@@ -4,96 +4,120 @@ import android.app.Activity;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
-import android.content.Intent;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.ImageButton;
 import android.widget.ImageView;
-import android.widget.RadioButton;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.cardview.widget.CardView;
-import androidx.core.content.res.ResourcesCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.bottomsheet.BottomSheetBehavior;
 import com.google.android.material.bottomsheet.BottomSheetDialog;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 import com.team.myapplication.R;
+import com.team.myapplication.adapter.DescriptionAdapter;
+import com.team.myapplication.adapter.DetailsAdapter;
+import com.team.myapplication.adapter.ProductAdpater;
 
-public class ProductDialogFragment extends BottomSheetDialogFragment {
+public class ProductDialogFragment extends BottomSheetDialogFragment implements DetailsAdapter.DescriptionClickListener {
 
-  public static final String TAG = "ActionBottomDialog";
-  private Context context;
-  private ItemClickListene mListener;
-  @Nullable
-  @Override
-  public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
-                           @Nullable Bundle savedInstanceState) {
-    return inflater.inflate(R.layout.bottom_sheet, container, false);
-  }
+    public static final String TAG = "ProductDialogFragment";
+    private Context context;
+    private ItemClickListene mListener;
+    private ImageView mainImage;
+    private RecyclerView productRV;
 
-  @Override
-  public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-    super.onViewCreated(view, savedInstanceState);
-    init(view);
-
-  }
-
-  @NonNull @Override public Dialog onCreateDialog(Bundle savedInstanceState) {
-    Dialog dialog = super.onCreateDialog(savedInstanceState);
-    dialog.setOnShowListener(new DialogInterface.OnShowListener() {
-      @Override public void onShow(DialogInterface dialogInterface) {
-        BottomSheetDialog bottomSheetDialog = (BottomSheetDialog) dialogInterface;
-        setupFullHeight(bottomSheetDialog);
-      }
-    });
-    return  dialog;
-  }
-  private void setupFullHeight(BottomSheetDialog bottomSheetDialog) {
-    FrameLayout bottomSheet = (FrameLayout) bottomSheetDialog.findViewById(R.id.design_bottom_sheet);
-    BottomSheetBehavior behavior = BottomSheetBehavior.from(bottomSheet);
-    ViewGroup.LayoutParams layoutParams = bottomSheet.getLayoutParams();
-
-    int windowHeight = getWindowHeight();
-    if (layoutParams != null) {
-      layoutParams.height = windowHeight-(windowHeight/10);
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.bottom_sheet, container, false);
     }
-    bottomSheet.setLayoutParams(layoutParams);
-    behavior.setState(BottomSheetBehavior.STATE_EXPANDED);
-  }
 
-  private int getWindowHeight() {
-    // Calculate window height for fullscreen use
-    DisplayMetrics displayMetrics = new DisplayMetrics();
-    ((Activity) getContext()).getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
-    return displayMetrics.heightPixels;
-  }
-  private void init(View view) {
-
-  }
-
-  @Override
-  public void onAttach(Context context) {
-    super.onAttach(context);
-    this.context =  context;
-    mListener = (ItemClickListene) context;
-  }
-
-  @Override
-  public void onDetach() {
-    super.onDetach();
-    mListener = null;
-  }
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        init(view);
+        configureRV();
+        mainImage.setClipToOutline(true);
 
 
-  public interface ItemClickListene {
-    void onItemClick(int position);
-  }
+    }
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        Dialog dialog = super.onCreateDialog(savedInstanceState);
+        dialog.setOnShowListener(new DialogInterface.OnShowListener() {
+            @Override
+            public void onShow(DialogInterface dialogInterface) {
+                BottomSheetDialog bottomSheetDialog = (BottomSheetDialog) dialogInterface;
+                setupFullHeight(bottomSheetDialog);
+            }
+        });
+        return dialog;
+    }
+
+    private void setupFullHeight(BottomSheetDialog bottomSheetDialog) {
+        FrameLayout bottomSheet = (FrameLayout) bottomSheetDialog.findViewById(R.id.design_bottom_sheet);
+        BottomSheetBehavior behavior = BottomSheetBehavior.from(bottomSheet);
+        ViewGroup.LayoutParams layoutParams = bottomSheet.getLayoutParams();
+
+        int windowHeight = getWindowHeight();
+        if (layoutParams != null) {
+            layoutParams.height = windowHeight - (windowHeight / 10);
+        }
+        bottomSheet.setLayoutParams(layoutParams);
+        behavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+    }
+
+    private int getWindowHeight() {
+        // Calculate window height for fullscreen use
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        ((Activity) getContext()).getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        return displayMetrics.heightPixels;
+    }
+
+    private void init(View view) {
+        mainImage = view.findViewById(R.id.imageView8);
+        productRV = view.findViewById(R.id.productRV);
+    }
+
+    private void configureRV() {
+        productRV.setLayoutManager(new LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false));
+        DetailsAdapter adapter = new DetailsAdapter(context, this);
+        productRV.setAdapter(adapter);
+
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        this.context = context;
+        mListener = (ItemClickListene) context;
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    @Override
+    public void position(int position) {
+
+
+    }
+
+
+    public interface ItemClickListene {
+        void onItemClick(int position);
+    }
 }
